bc07adb51279164810c36b0b484ad2039896f5ff
Jed Peek <jedpeek@yahoo.com>
2018-08-28 22:51:34 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.find_by(name: params[:name])
     user = user.try(:authenticate, params[:user][:password])
     return redirect_to(controller: 'sessions', action: 'new') unless user
-
+    session[:user_id] = user.id
     return redirect_to controller:'users', action:'index'
   end
 end
