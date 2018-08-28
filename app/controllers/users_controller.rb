a5acab4d9c4a22fb5112b42f1284c134ce467dca
Jed Peek <jedpeek@yahoo.com>
2018-08-28 22:22:01 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,13 @@ class UsersController < ApplicationController
   end
 
   def create
+    user = User.new(user_params).save
   end
+ 
+  private
+ 
+  def user_params
+    params.require(:user).permit(:username, :password, :password_confirmation)
+  end
+end
 end
