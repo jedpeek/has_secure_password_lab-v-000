48a690a59ec340e24e3f29f2a74e1e85ab647e40
Jed Peek <jedpeek@yahoo.com>
2018-08-28 22:36:42 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
 
-  # You can have the root of your site routed with "root"
+  resources :users, only: [:new, :create]
    root 'users#index'
    get '/signup' => 'users#index'
    post '/signup' => 'users#create'
