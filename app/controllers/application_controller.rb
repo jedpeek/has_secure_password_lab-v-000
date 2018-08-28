212a7ca81a5c29892e4d2dc432df895356f5f184
Jed Peek <jedpeek@yahoo.com>
2018-08-28 22:53:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
+
   def require_logged_in
-  return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
-end
+    return redirect_to(controller: 'sessions', action: 'new') unless logged_in?
+  end
 end
