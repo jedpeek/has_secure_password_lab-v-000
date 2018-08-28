96a14a26650cddb8c0efc1525d516e65a38a33e6
Jed Peek <jedpeek@yahoo.com>
2018-08-28 22:33:02 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
    root 'users#index'
    get '/signup' => 'users#new'
+      post '/signup' => 'users#create'
    post '/signup' => 'sessions#create'
-   post '/signup' => 'users#create'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
